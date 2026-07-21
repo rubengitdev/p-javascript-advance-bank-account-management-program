098d47488164fa9f4decbcea37b9de924666feb4
rubengitdev <rubencahyadi504@gmail.com>
2026-07-21 19:28:50 +0700

    Added deposit method to BankAccount class

---
+++ script.js
@@ -0,0 +1,34 @@
+class BankAccount {
+    constructor(balance, transactions) {
+        this.balance = 0;
+        this.transactions = [];
+    }
+
+    deposit(depositAmount) {
+        if (depositAmount > 0) {
+            this.transactions.push({
+                type: "deposit",
+                amount: depositAmount,
+            });
+
+            this.balance += depositAmount;
+
+            return `Successfully deposited ${depositAmount}. New balance: ${this.balance}`;
+        }
+        return `Deposit amount must be greater than zero.`;
+    }
+
+    withdraw(withdrawalAmount) {
+        if (withdrawalAmount > 0 && withdrawalAmount >= this.balance) {
+            this.transactions.push({
+                type: "withdraw",
+                amount: withdrawalAmount,
+            });
+
+            this.balance += withdrawalAmount;
+
+            return `Successfully withdrew ${withdrawalAmount}. New balance: ${this.balance}`;
+        }
+        return `Insufficient balance or invalid amount.`;
+    }
+}
